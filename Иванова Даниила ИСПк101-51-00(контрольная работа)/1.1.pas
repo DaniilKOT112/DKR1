bd74285a220d952b7e27e9c9069f6ef056395656
DaniilKOT112 <117807275+DaniilKOT112@users.noreply.github.com>
2022-11-09 10:30:58 +0300

    Add files via upload

---
+++ Иванова Даниила ИСПк101-51-00(контрольная работа)/1.1.pas
@@ -0,0 +1,11 @@
+﻿var x:real;
+
+begin
+  readln(x);
+  if x < -7 then writeln(' y =', x ** (0.1 * x) * e**((x + x)/log(x)):0:2);
+  if (-7 <= x) and (x < 0) then writeln(' y=', sin(x)/cos(x):0:2);
+  if 0 <= x then writeln(' y=',sin(x)/cos(x):0:2);
+end. 
+
+
+
---
+++ Иванова Даниила ИСПк101-51-00(контрольная работа)/1.2.pas
@@ -0,0 +1,12 @@
+﻿var x, y: real;
+begin 
+  x:= -9;
+  while x <= 2 do
+    begin
+    if x < -7 then  y:= x ** (0.1 * x) * e**((x + x)/log(x));
+    if (-7 <= x) and (x < 0) then  y:= sin(x)/cos(x);
+    if 0 <= x then y:= sin(x)/cos(x);
+    writeln('y = ', y:0:2);
+    x += 0.1;
+    end;
+end.
